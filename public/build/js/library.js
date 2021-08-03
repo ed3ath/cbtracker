165ee9b695e36fd1ec95f057a125397ff579093d
ed3ath <edeath1208@gmail.com>
2021-08-03 10:03:09 +0800

    updates

--- public/build/js/library.js
+++ public/build/js/library.js
@@ -53,7 +53,6 @@ const randomString = (length) => {
 };
 
 function CharacterPower(level) {
-    return (1000 + ((level - 1) * 10)) * (Math.floor((level - 1) / 10) + 1);
     return ((1000 + (level * 10)) * (Math.floor(level / 10) + 1));
 }
 
