71ca0f756c7b8f9fda09a818328e3cb3126ac62a
ed3ath <edeath1208@gmail.com>
2021-08-05 20:02:00 +0800

    sorting

--- public/build/js/market.js
+++ public/build/js/market.js
@@ -4,12 +4,12 @@ async function loadWeaponListing() {
     $('.btn-refresh').prop('disabled', true)
     $.get('/listing/weapons', async result => {
         $table.html('')
-        if(result.length > 0) {
+        if (result.length > 0) {
             $table.append(await Promise.all(result.map(async weapId => {
                 const weapData = weaponFromContract(weapId, await getWeaponData(weapId))
                 const price = await getFinalPrice(weapAddress, weapId)
                 if (parseFloat(price) > 0) {
-                    let stars = ''; let attr = '';                    
+                    let stars = ''; let attr = '';
                     for (let i = 0; i <= weapData.stars; i++) {
                         stars += '<img class="me-1" src="/img/star.svg" alt="Star" width="20">';
                     }
@@ -26,7 +26,7 @@ async function loadWeaponListing() {
                         attr += `<br>BONUS +${weapData.bonusPower}`;
                     }
                     return `
-                        <tr>
+                        <tr data-stars="${weapData.stars}">
                             <td class="align-middle text-white">${weapId}</td>
                             <td class="align-middle text-white">
                                 <div class="d-flex align-items-center">
@@ -46,6 +46,7 @@ async function loadWeaponListing() {
         } else {
             $table.append('<tr><td class="text-center text-white" colspan="5">No weapon listed</td></tr>')
         }
+        sortTable()
         $('.btn-refresh').removeAttr('disabled')
     })
 }
@@ -72,6 +73,29 @@ function traitToIcon(traitNum) {
         case WeaponTrait.CHA: return 'lightning.png';
         case WeaponTrait.PWR: return 'pwr.svg';
         default: return '???';
+    }
+}
+
+function sortTable() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("table-weapons");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = $(rows[i]).data('stars');
+            y = $(rows[i + 1]).data('stars');
+            if (parseInt(x) > parseInt(y)) {
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
     }
 }
 
