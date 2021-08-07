a4d5a20c32854a788b7eee25e84ddbe01b528a16
ed3ath <edeath1208@gmail.com>
2021-08-08 00:28:33 +0800

    localhost

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use((err, req, res, next) => {
 
 const httpServer = http.createServer(app);
 const httpPort = config.port || 3000;
-httpServer.listen(httpPort, () => {
+httpServer.listen(httpPort, 'localhost', () => {
   console.log(`HTTP Server running on port ${httpPort}`);
 });
 
